aaa588193c2bcd466144eca51c242383b189e3b0
yashmadhwal <yashmadhwal@yahoo.co.in>
2021-05-19 14:50:50 +0300

    After lots of research when talking about payable, I realised that Truffle was using version 0.5, whereas I required 0.6

--- truffle-config.js
+++ truffle-config.js
@@ -80,9 +80,9 @@ module.exports = {
   },
 
   // Configure your compilers
-  // compilers: {
-  //   solc: {
-  //      version: "^0.8.0",    // Fetch exact version from solc-bin (default: truffle's version)
+  compilers: {
+    solc: {
+       version: "0.6.4",    // Fetch exact version from solc-bin (default: truffle's version)
   //     // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
   //     // settings: {          // See the solidity docs for advice about optimization and evmVersion
   //     //  optimizer: {
@@ -91,6 +91,6 @@ module.exports = {
   //     //  },
   //     //  evmVersion: "byzantium"
   //     // }
-  //   }
-  // }
+    }
+  }
 };
